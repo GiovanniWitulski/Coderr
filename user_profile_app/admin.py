bf5c0cd8e076c0c5632bd4c5710595d9b2cee3d9
GiovanniWitulski <witulski.schule@gmail.com>
2025-10-17 00:17:53 +0200

    feat: Add user profile model to admin panel

--- user_profile_app/admin.py
+++ user_profile_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'type')
+    list_filter = ('type',)
+    search_fields = ('user__username',)
