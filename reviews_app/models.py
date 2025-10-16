a06918eaf53e6af66d61ea83d60ea92518376958
GiovanniWitulski <witulski.schule@gmail.com>
2025-10-17 00:13:19 +0200

    feat: Add models for reviews

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from user_profile_app.models import UserProfile
 
-# Create your models here.
+class Review(models.Model):
+    business_user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name='reviews_received',
+        limit_choices_to={'type': UserProfile.UserType.BUSINESS}
+    )
+    
+    reviewer = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name='reviews_written',
+        limit_choices_to={'type': UserProfile.UserType.CUSTOMER}
+    )
+    
+    rating = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    
+    description = models.TextField()
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('business_user', 'reviewer')
+
+    def __str__(self):
+        return f"Review from {self.reviewer.user.username} for {self.business_user.user.username} ({self.rating} stars)"
