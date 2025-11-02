bc114d270ed276bdf25deb9e8223b9a095272a7c
GiovanniWitulski <witulski.schule@gmail.com>
2025-11-02 21:28:15 +0100

    feat(orders): add urlpatterns

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet
+from .views import OrderViewSet, OrderCountView, CompletedOrderCountView
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='order')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('order-count/<int:business_user_id>/', OrderCountView.as_view(), name='order-count'),
+    path('completed-order-count/<int:business_user_id>/', CompletedOrderCountView.as_view(), name='completed-order-count'),
+]
