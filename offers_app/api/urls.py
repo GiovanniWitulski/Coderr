42ac9cddd15da234a34ebd63676455f29ed86b7b
GiovanniWitulski <witulski.schule@gmail.com>
2025-11-02 21:17:49 +0100

    feat: add offers urls to core

--- offers_app/api/urls.py
+++ offers_app/api/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import OfferViewSet
+from .views import OfferDetailViewSet, OfferViewSet
 
 router = DefaultRouter()
 router.register(r'offers', OfferViewSet, basename='offer')
+router.register(r'offerdetails', OfferDetailViewSet, basename='offerdetail')
 
 urlpatterns = router.urls
