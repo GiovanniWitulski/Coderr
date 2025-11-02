aafecc43c4df5fe733c132262f703c48562e7e05
GiovanniWitulski <witulski.schule@gmail.com>
2025-11-02 21:30:20 +0100

    feat(reviews): add review view

--- reviews_app/api/views.py
+++ reviews_app/api/views.py
@@ -1,25 +1,27 @@
-from rest_framework import viewsets, permissions, mixins
+# reviews_app/api/views.py
+
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny, IsAuthenticated 
 from ..models import Review
 from .serializers import ReviewSerializer
-
-class IsCustomerUser(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return (
-            request.user.is_authenticated and
-            hasattr(request.user, 'profile') and
-            request.user.profile.type == 'customer'
-        )
+from .permissions import IsCustomerUser
 
 class ReviewViewSet(mixins.CreateModelMixin,   
-                    mixins.ListModelMixin,     
-                    mixins.RetrieveModelMixin, 
-                    mixins.UpdateModelMixin,   
-                    mixins.DestroyModelMixin,  
-                    viewsets.GenericViewSet):
+                   mixins.ListModelMixin,     
+                   mixins.RetrieveModelMixin, 
+                   mixins.UpdateModelMixin,   
+                   mixins.DestroyModelMixin,  
+                   viewsets.GenericViewSet):
 
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [permissions.IsAuthenticated, IsCustomerUser]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [IsAuthenticated, IsCustomerUser] 
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+            
+        return [permission() for permission in permission_classes]
