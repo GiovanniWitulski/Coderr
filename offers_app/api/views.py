c9e5b88fc0ad50bc13cc5dfff5ef431730d1343e
GiovanniWitulski <witulski.schule@gmail.com>
2025-11-02 21:18:49 +0100

    feat: add offers detail view

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,27 +1,31 @@
-from rest_framework import viewsets, permissions
-from rest_framework.permissions import IsAuthenticated
-from offers_app.api.permissions import IsOwnerOrReadOnly
-from ..models import Offer
-from .serializers import OfferSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from offers_app.api.permissions import IsOwnerOrReadOnly, IsBusinessUser
+from ..models import Offer, OfferDetail
+from .serializers import OfferDetailSerializer, OfferSerializer
+from rest_framework import permissions
 
-class IsBusinessUser(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return (request.user.is_authenticated and hasattr(request.user, 'profile') and 
-            request.user.profile.type == 'business')
 
 class OfferViewSet(viewsets.ModelViewSet):
-    queryset = Offer.objects.all()
+    queryset = Offer.objects.all().order_by('-updated_at')
     serializer_class = OfferSerializer
+    
     def get_permissions(self):
         if self.action in ['update', 'partial_update', 'destroy']:
             permission_classes = [IsOwnerOrReadOnly]
         elif self.action == 'create':
             permission_classes = [IsBusinessUser]
+        elif self.action in ['list', 'retrieve']: 
+            permission_classes = [AllowAny]
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user.profile)
+
+
+class OfferDetailViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = OfferDetail.objects.all()
+    serializer_class = OfferDetailSerializer
+    permission_classes = [permissions.AllowAny]
