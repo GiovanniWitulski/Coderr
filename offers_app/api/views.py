74575d2dae2fceb39c18db9697fbd653068f642f
GiovanniWitulski <witulski.schule@gmail.com>
2025-11-11 23:45:31 +0100

    feat(OfferViewSet): add 'filter backends' and 'filterset class'

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -8,10 +8,14 @@ Provides the viewsets for:
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from offers_app.api.filters import OfferFilter
 from offers_app.api.permissions import IsOwnerOrReadOnly, IsBusinessUser
 from ..models import Offer, OfferDetail
 from .serializers import OfferDetailSerializer, OfferSerializer
-from rest_framework import permissions
+from .pagination import StandardResultsSetPagination 
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, filters
+from django.db.models import Min
 
 
 class OfferViewSet(viewsets.ModelViewSet):
@@ -20,23 +24,37 @@ class OfferViewSet(viewsets.ModelViewSet):
     Permissions are handled dynamically based on the action.
     """
 
-    # Default sorting is newest 'updated_at' first.
-    queryset = Offer.objects.all().order_by('-updated_at')
     serializer_class = OfferSerializer
+    pagination_class = StandardResultsSetPagination 
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
     
+    filterset_class = OfferFilter 
+    search_fields = ['title', 'description']
+    ordering_fields = ['updated_at', 'min_price']
+    ordering = ['-updated_at']
+
+    def get_queryset(self):
+        return Offer.objects.annotate(
+            min_price=Min('details__price'),
+            min_delivery_time=Min('details__delivery_time_in_days')
+        ).all().order_by('-updated_at')
+
     def get_permissions(self):
         """
         Dynamically assigns permissions based on the request action.
         """
-
         if self.action in ['update', 'partial_update', 'destroy']:
             permission_classes = [IsOwnerOrReadOnly]
         elif self.action == 'create':
             permission_classes = [IsBusinessUser]
-        elif self.action in ['list', 'retrieve']: 
+        elif self.action in ['list', 'retrieve']:
             permission_classes = [AllowAny]
         else:
-            # Fallback to a safe default (must be authenticated)
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
 
@@ -45,7 +63,6 @@ class OfferViewSet(viewsets.ModelViewSet):
         Overrides the create behavior to automatically assign the logged-in
         user's profile as the 'creator' of the new offer.
         """
-
         serializer.save(creator=self.request.user.profile)
 
 
